2cac69b1c1a6cc55143e6a2a5ffd52e1deabb613
andrew54068 <andrew0424718012@yahoo.com.tw>
2019-09-17 09:45:14 +0800

    [feat] load png image.

--- ImageMemoryOptimizeDemo/ViewController.swift
+++ ImageMemoryOptimizeDemo/ViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView.image = UIImage(named: "PNG_800_600")!
     }
 
-
 }
-
---
+++ ImageMemoryOptimizeDemo/a.swift
@@ -0,0 +1,9 @@
+//
+//  a.swift
+//  ImageMemoryOptimizeDemo
+//
+//  Created by kidnapper on 2019/9/17.
+//  Copyright © 2019 andrew54068. All rights reserved.
+//
+
+import Foundation
