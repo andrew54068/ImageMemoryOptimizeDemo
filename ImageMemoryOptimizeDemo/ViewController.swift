4b8eec8d45636d64817788c03a527d31830b6a06
andrew54068 <andrew0424718012@yahoo.com.tw>
2019-09-17 09:51:47 +0800

    [feat] down sample with png.

--- ImageMemoryOptimizeDemo/ViewController.swift
+++ ImageMemoryOptimizeDemo/ViewController.swift
@@ -15,7 +15,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        imageView.image = UIImage(named: "jpeg_1280_720")!
+        let path = Bundle.main.path(forResource: "PNG_transparency_demonstration_1.png" , ofType: "png")!
+        let fileURL = URL(fileURLWithPath: path)
+        
+        imageView.image = downsample(imageAt: fileURL, maxDimentionInPixels: 100)
     }
 
+}
+
+func downsample(imageAt imageURL: URL, maxDimentionInPixels: CGFloat) -> UIImage {
+    let imageSourceOptions = [kCGImageSourceShouldCache: false] as CFDictionary
+    let imageSource = CGImageSourceCreateWithURL(imageURL as CFURL, imageSourceOptions)!
+    let downsampledOptions = [kCGImageSourceCreateThumbnailFromImageAlways: true,
+                              kCGImageSourceShouldCacheImmediately: true,
+                              kCGImageSourceCreateThumbnailWithTransform: true,
+                              kCGImageSourceThumbnailMaxPixelSize: maxDimentionInPixels] as CFDictionary
+    
+    let downsampledImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, downsampledOptions)!
+    
+    return UIImage(cgImage: downsampledImage)
 }
